837f222636b4f53ad54586e85182320ffeb3b684
viniciusviana73 <54729152+viniciusviana73@users.noreply.github.com>
2023-01-18 22:38:58 -0300

    menu fix

--- assets/script/menu.js
+++ assets/script/menu.js
@@ -1,12 +1,19 @@
+const burger = document.querySelector('span#burger')
+
 function clickMenu() {
-    topmenu.style.display == 'block' ?
-    topmenu.style.display = 'none' : 
-    topmenu.style.display = 'block'
+    if (topmenu.style.display == 'block') {
+        topmenu.style.display = 'none'
+        burger.innerHTML = 'menu'
+    } else {
+        topmenu.style.display = 'block'
+        burger.innerHTML = 'close'
+    }
 }
 
 function changeSize() {
     if (window.innerWidth >= 600) {
         topmenu.style.display = 'block'
+        burger.innerHTML = 'menu'
     } else {
         topmenu.style.display = 'none'
     }
