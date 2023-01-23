98f842f99ad5ec551f4feb8161d23aa9902721ce
viniciusviana73 <54729152+viniciusviana73@users.noreply.github.com>
2023-01-23 16:39:35 -0300

    light / dark theme switch button

---
+++ assets/script/colors.js
@@ -0,0 +1,16 @@
+const mode = document.querySelector('#cmode')
+const header = document.querySelector('header')
+const menu = document.querySelector('menu')
+const sections = document.querySelectorAll('section')
+const footer = document.querySelector('footer')
+
+mode.onclick = () => {
+    document.body.classList.toggle('light')
+    header.classList.toggle('light')
+    menu.classList.toggle('light')
+    burger.classList.toggle('light')
+    for (let p = 0; p < sections.length; p++) {
+        sections[p].classList.toggle('light')
+    }
+    footer.classList.toggle('light')
+}
